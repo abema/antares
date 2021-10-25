e0b12f890a3497c6f19d415de95f66a2ac368c0b
Kubo Ryosuke <sunfish-shogi@users.noreply.github.com>
2021-10-25 12:42:41 +0900

    fix for go-vet errors

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build:
 
 .PHONY: vet
 vet:
-	go vet
+	go vet ./...
 
 .PHONY: lint
 lint:
--- core/file.go
+++ core/file.go
@@ -10,14 +10,14 @@ type Meta struct {
 	Via              string        `json:"via"`
 	RequestHeader    http.Header   `json:"requestHeader"`
 	ResponseHeader   http.Header   `json:"responseHeader"`
-	Status           string        `json:"status`     // e.g. "200 OK"
-	StatusCode       int           `json:"statusCode` // e.g. 200
-	Proto            string        `json:"proto`      // e.g. "HTTP/1.0"
-	ProtoMajor       int           `json:"protoMajor` // e.g. 1
-	ProtoMinor       int           `json:"protoMinor` // e.g. 0
-	ContentLength    int64         `json:"contentLength`
+	Status           string        `json:"status"`     // e.g. "200 OK"
+	StatusCode       int           `json:"statusCode"` // e.g. 200
+	Proto            string        `json:"proto"`      // e.g. "HTTP/1.0"
+	ProtoMajor       int           `json:"protoMajor"` // e.g. 1
+	ProtoMinor       int           `json:"protoMinor"` // e.g. 0
+	ContentLength    int64         `json:"contentLength"`
 	TransferEncoding []string      `json:"transferEncoding"`
-	Uncompressed     bool          `json:"uncompressed`
+	Uncompressed     bool          `json:"uncompressed"`
 	RequestTimestamp time.Time     `json:"requestTimestamp"`
 	DownloadTime     time.Duration `json:"downloadTime"`
 }
